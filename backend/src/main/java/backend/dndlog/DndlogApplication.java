ad918a97bf857779ea70364b4530d790f0785841
Jenny <205511984+jenny492@users.noreply.github.com>
2025-09-20 21:24:15 +0300

    Kansiorakenteen muutos, backendin lisäys

---
+++ backend/src/main/java/backend/dndlog/DndlogApplication.java
@@ -0,0 +1,13 @@
+package backend.dndlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndlogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/dndlog/DndlogApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.dndlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
